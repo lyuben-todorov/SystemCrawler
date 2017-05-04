c138a049ad0b10e4ae473f068c5bff6c277f63ab
kappa-4head <lyuben.todorov@gmail.com>
2017-05-04 23:19:48 +0300

    Initial commit

---
+++ SystemsDB/SystemsDB/RESTRequest.cs
@@ -0,0 +1,26 @@
+﻿using RestSharp;
+
+namespace Systems
+{
+    public class Requests
+    {
+        public static string POSTRequest(string BaseURL, string ResourceURI, string data)
+        {
+            var client = new RestClient(BaseURL);
+            var request = new RestRequest(ResourceURI, Method.POST);
+            request.AddParameter("data", data, ParameterType.RequestBody);
+            request.RequestFormat = DataFormat.Json;
+            var response = client.Execute(request);
+            return response.Content.ToString();
+        }
+        public static string GETRequest(string BaseURL, string ResourceURI)
+        {
+            var client = new RestClient(BaseURL);
+            var request = new RestRequest(ResourceURI, Method.GET);
+            request.OnBeforeDeserialization = resp => { resp.ContentType = "application/json"; };
+            var response = client.Execute(request);
+            return response.Content.ToString();
+        }
+
+    }
+}
---
+++ SystemsDB/SystemsDB/SystemCrawler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+namespace Systems
+{
+    class Systems
+    {
+        public static List<string> VistitedSystems = new List<string>();
+        static void Main(string[] args)
+        {
+            SystemCrawler("30000142");
+            Console.Read();
+        }
+        public static void SystemCrawler(string SystemID)
+        {
+            VistitedSystems.Add(SystemID);
+            foreach (string System in SystemConnectsTo(SystemID))
+            {
+                Console.Write("\r{0} Systems parsed", VistitedSystems.Count);
+                if (!VistitedSystems.Contains(System))SystemCrawler(System);
+            }
+        }
+        public static List<string> SystemConnectsTo(string SystemID) {
+            JObject jSystem = JObject.Parse(Requests.GETRequest(@"https://esi.tech.ccp.is", string.Format("/latest/universe/systems/{0}/?datasource=tranquility&language=en-us", SystemID)));
+            List<string> Gates = new List<string>();
+            foreach (var element in jSystem["stargates"])
+            {
+                Gates.Add(element.ToString());
+            }
+            List<string> Systems = new List<string>();
+            foreach(string gate in Gates)
+            {
+                Systems.Add(GetSystemFromGate(gate));
+            }
+            return Systems;
+        }
+        public static string GetSystemFromGate(string GateID)
+        {
+            JObject jGate = JObject.Parse(Requests.GETRequest(@"https://esi.tech.ccp.is", string.Format(@"/latest/universe/stargates/{0}/?datasource=tranquility", GateID)));
+            return jGate["destination"]["system_id"].ToString();
+        }
+        public static string GetName(string SystemID)
+        {
+            JObject jSystem = JObject.Parse(Requests.GETRequest(@"https://esi.tech.ccp.is", string.Format("/latest/universe/systems/{0}/?datasource=tranquility&language=en-us", SystemID)));
+            return jSystem["name"].ToString();
+        }
+    }
+}
